ec398c96a2d87e71292a97bac45664a0981006f0
George Bai <georgebai86@gmail.com>
2024-12-13 23:26:29 +1000

    remove expand

--- st_link_analysis/component/events.py
+++ st_link_analysis/component/events.py
@@ -2,7 +2,7 @@
 For more details refer to https://js.cytoscape.org/#events
 """
 
-RESERVED_NAMES = ["remove", "expand"]
+# RESERVED_NAMES = ["remove", "expand"]
 
 class Event:
     def __init__(
@@ -34,8 +34,6 @@ class Event:
         self.name = name
         self.event_type = event_type
         self.selector = selector
-        if name in RESERVED_NAMES:
-            raise ValueError(f"{RESERVED_NAMES} are reserved action names")
 
     def dump(self) -> dict:
         return {
